1ce8af431d06eb077036934703295a6e82b8c786
Karim Abdul <kforkarim@gmail.com>
2015-10-25 21:08:30 -0400

    1) Completed MainVC

--- VoluteeRide/VoluteeRide/ViewController.m
+++ VoluteeRide/VoluteeRide/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *signUpBtn;
+@property (weak, nonatomic) IBOutlet UIButton *logInBtn;
+
 @end
 
 @implementation ViewController
@@ -17,11 +20,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.signUpBtn.layer.cornerRadius = 5; // this value vary as per your desire
+    self.signUpBtn.clipsToBounds = YES;
+    
+    self.logInBtn.layer.cornerRadius = 5; // this value vary as per your desire
+    self.logInBtn.clipsToBounds = YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
